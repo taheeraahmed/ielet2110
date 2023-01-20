26798cb4c9883d46ac853b0ace227c40d944c506
taheeraahmed <taheera@hotmail.com>
2023-01-20 12:42:55 +0100

    Did stuff on task 2

--- ø2/task_2.m
+++ ø2/task_2.m
@@ -1,9 +1,41 @@
-%a
-t = 0:0.05:2;
+% a
+stop = 0.002;             
+t = (0:0.000005:stop)';     
 
-%b
+% b
 f = 2000;
-A = 5;
-s = A*sin(2*pi*f*t);
-plot(t,s)
+x = 5*sin(2*pi*f*t);
 
+figure;
+plot(t,x);
+xlabel('time (s)');
+set(gca,'xtick', 0:0.0001:stop)
+title('Signal versus Time');
+zoom xon;
+
+%c 
+% ????
+
+% d 
+
+step1 = 0.0000005;
+step2 = 0.002;
+step3 = 0.0025;
+step4 = 0.004;
+
+t1 = (0:step1:stop);  
+t2 = (0:step2:stop);  
+t3 = (0:step3:stop);  
+t4 = (0:step4:stop);  
+
+% Create plots
+figure;
+t = tiledlayout(2,2);
+nexttile
+plot(t1,x)
+nexttile
+plot(t2,x)
+nexttile
+plot(t3,x)
+nexttile
+plot(t4,x)
