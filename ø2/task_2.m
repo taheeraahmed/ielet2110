a0ea52f4629467c773c896473334e40147cc3ac5
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2023-01-31 09:31:47 +0100

    Fixed task 2

--- ø2/task_2.m
+++ ø2/task_2.m
@@ -9,7 +9,7 @@ x = 5*sin(2*pi*f*t);
 figure;
 plot(t,x);
 set(gca,'xtick', 0:0.0001:stop)
-title('Signal versus Time');
+title('Task 2b: Signal versus Time');
 zoom xon;
 
 %c 
@@ -20,12 +20,26 @@ Ax.XLabel.String = 'Time (ms)';
 
 % d 
 
-%step = [0.000005 0.002 0.0025 0.004];
+stop = 2e-3;
+f = 2000;
+step = [50e-6 0.2e-3 0.25e-3 0.4e-3];
 
-%hold on;
-%for i = 1:size(step)
-%    test = (0:step(i):stop);
-%    figure;
-%    plot(test,x);
-%end
-%hold off;
+t_d1 = 0:step(1):stop;
+t_d2 = 0:step(2):stop;
+t_d3 = 0:step(3):stop;
+t_d4 = 0:step(4):stop;
+
+%Top two plots
+x_d1 = 5*sin(2*pi*f*t_d1);
+x_d2 = 5*sin(2*pi*f*t_d2);
+x_d3 = 5*sin(2*pi*f*t_d3);
+x_d4 = 5*sin(2*pi*f*t_d4);
+
+figure;
+title('Task 2d')
+hold on;
+plot(t_d1 * 1e-3,x_d1);
+plot(t_d2 * 1e-3,x_d2);
+plot(t_d3 * 1e-3,x_d3);
+plot(t_d4 * 1e-3,x_d4);
+hold off;
